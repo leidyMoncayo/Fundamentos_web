f6b2d67f8227d2f4ed9b6d279696afa0607dc4fe
leidyMoncayo <ljmoncayo@estudiante.uniajc.edu.co>
2025-08-22 17:28:08 +0000

    a

---
+++ ptogramacion uwu/clase2/Particiones/javascript/ejercicio3.js
@@ -0,0 +1,9 @@
+const datosImagen =[
+{id:"img1", src:"./imgs/ropa1.jpg", alt:"Imagen 1",width:"300"},
+{id:"img1", src:"./imgs/ropa1.jpg", alt:"Imagen 1",width:"300"},
+{id:"img1", src:"./imgs/ropa1.jpg", alt:"Imagen 1",width:"300"}
+
+];
+function crearImagen(src, alt, width){
+    return '<img src="${src}" alt="${alt} "width="${width}>';s
+}
